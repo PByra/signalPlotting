db997952a3ebff4ff76cb3bf6fadf595b76f9d6a
Patrick Byra <patrickbyra@gmail.com>
2025-05-02 10:02:42 -0500

    Created function to generate random signal and plot it before performing any modulation or filtering. Going to comment and make changes to be in line with PEP8 guidelines. Name change of project will also occur as scope has changed as well

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-# Note for later: Create a script that asks the user what type of signal you want 
-# to model and plot. We need to create another python script for functions that 
-# that can do this. Then we need the user to input the parameters for the signal.
-# finally import matplotlib and plot the signal. I would like to model this signal
-# with different shifts, filters, etc.
+import signals
 
 def main():
-
+    signal, frequency, modulation_index, time, phase = signals.general_signal()
+    signals.plot_signal(signal, time = time, title = "Random Signal")
 
 if __name__ == "__main__":
     main()
--- signals.py
+++ signals.py
@@ -1,11 +1,19 @@
 #Defining of Signals based on parameters 
-
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
+def general_signal():
+    frequency = np.random.uniform(3000, 4000)  
+    modulation_index = np.random.uniform(0, 1) 
+    phase = np.random.uniform(0, 2 * np.pi)
+    time = np.arange(0, 1000, 2)
+    signal  = np.sin(2 * np.pi * frequency * time + phase)
+    print(len(signal))    
+    return signal, frequency, modulation_index, time, phase
 
 def amplitude_modulated_signal(frequency, modulation_index, time):
     carrier = np.cos(2 * np.pi * frequency * time)
-    modulating_signal = 1 + modulation_index * np.cos(2 * np.pi * frequency * time)
+    modulating_signal = 1 + modulation_index * carrier
     return carrier * modulating_signal
 
 def frequency_modulated_signal(frequency, modulation_index, time):
@@ -13,3 +21,16 @@ def frequency_modulated_signal(frequency, modulation_index, time):
     instantaneous_frequency = frequency + modulation_index * modulating_signal
     return np.cos(2 * np.pi * instantaneous_frequency * time)
 
+def phase_modulated_signal(frequency, modulation_index, time):
+    modulating_signal = np.cos(2 * np.pi * frequency * time)
+    instantaneous_phase = 2 * np.pi * frequency * time + modulation_index * modulating_signal
+    return np.cos(instantaneous_phase)
+
+def plot_signal(signal, time, title):
+    plt.figure(figsize=(10, 4))
+    plt.plot(time, signal)
+    plt.title(title)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude')
+    plt.grid()
+    plt.show()
